60465eb8fe29ed90f9ac1bd18b689fcdf0301853
Justin Luciano <justinlluciano@gmail.com>
2021-12-15 15:20:17 -0800

    commit tree b3bb0f213ab4076a26e491432abdebae871c68b8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const writeCards = (names, eventName) => {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(
+      `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return messages;
+}
+
+const countDown = (num) => {
+  let counter = num;
+  while (counter >= 0) {
+    console.log(counter);
+    counter--;
+  }
+}
+
+countDown(10);
